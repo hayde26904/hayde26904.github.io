3e88c3e3df5f9bf6b0ef2e83d9fbfde7438ea688
hayde26904 <hayde26904@live.ytech.edu>
2023-11-08 08:54:35 -0500

    Update index.js

--- coolgame/index.js
+++ coolgame/index.js
@@ -19,7 +19,7 @@ let shootCoolDown = shootCoolDownTime;
 
 class Particle {
 
-    constructor(x,y,w,h,c,xv,yv,g){
+    constructor(x, y, w, h, c, xv, yv, g) {
 
         this.x = x;
         this.y = y;
@@ -29,38 +29,48 @@ class Particle {
 
         this.xv = xv;
         this.yv = yv;
+        this.xaccel = 1;
+        this.yaccel = 1;
         this.gravity = g;
 
-        this.counter = 0;
-        //this.expire = 20;
+        this.counter = 50;
+
+        this.onGround = false;
 
     }
 
-    draw(){
+    draw(ctx) {
 
-        stage.fillStyle = this.color;
-        stage.fillRect(this.x, this.y, this.w, this.h);
-        stage.fillStyle = '#000';
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+        ctx.fillStyle = '#000';
 
     }
 
-    physics(){
-
+    physics() {
+        //if(this.willStay) this.counter--;
         this.yv += this.gravity;
 
-        this.x+=this.xv;
-        this.y+=this.yv;
+        this.x += Math.round(this.xv);
+        if(!this.onGround) this.y += Math.round(this.yv);
 
+        if (this.xv < 0 && this.onGround) {
+            this.xv+=0.1;
+        } else if (this.xv > 0 && this.onGround) {
+            this.xv-=0.1;
+        }
     }
 
 }
 
 class Enemy {
-    constructor(x, y, w, h, maxhealth){
+    constructor(x, y, w, h, maxhealth) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+
+        this.color = '#FF0000';
 
         this.maxHealth = maxhealth;
         this.health = this.maxHealth;
@@ -68,21 +78,22 @@ class Enemy {
         this.dead = false;
     }
 
-    draw(ctx){
-        ctx.fillStyle = '#FF0000';
+    draw(ctx) {
+        ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.w, this.h);
-
-        if(!this.dead){
-            ctx.fillRect(this.x - this.w/10, this.y - this.h/3, this.w + (this.w/10)* 2, this.h/10);
+        ctx.fillStyle = '#FF0000';
+        if (!this.dead) {
+            ctx.fillRect(this.x - this.w / 10, this.y - this.h / 3, this.w + (this.w / 10) * 2, this.h / 10);
             ctx.fillStyle = '#268b07';
-            ctx.fillRect(this.x - this.w/10, this.y - this.h/3, (this.health/this.maxHealth) * (this.w + (this.w/10)* 2), this.h/10);
+            ctx.fillRect(this.x - this.w / 10, this.y - this.h / 3, (this.health / this.maxHealth) * (this.w + (this.w / 10) * 2), this.h / 10);
         }
     }
 
-    update(){
-        if(this.health <= 0){
-            this.health = 0;
-            this.dead = true;
+    update() {
+        if (this.health <= 0) {
+            //this.health = 0;
+            //this.dead = true;
+            this.health = 100;
         }
     }
 }
@@ -107,7 +118,7 @@ class Bullet {
 
     draw(ctx) {
         ctx.beginPath();
-        ctx.moveTo(this.x - this.dirX/25, this.y - this.dirY/25);
+        ctx.moveTo(this.x - this.dirX / 25, this.y - this.dirY / 25);
         ctx.lineTo(this.x, this.y);
         ctx.lineWidth = 3;
         ctx.strokeStyle = "#ffce00";
@@ -119,14 +130,28 @@ class Bullet {
         this.y += this.dirY * this.spd;
     }
 
-    collision(obj){
+    collision(obj) {
         return this.x >= obj.x && this.x <= obj.x + obj.w && this.y >= obj.y && this.y <= obj.y + obj.h;
     }
 }
 
+function explode(size, x, y, w, h, color, g) {
+    explosion = [];
+    for (p = 0; p < size; p++) {
+        var xv = Math.floor(Math.random() * (10 - -10 + 1)) + -10;
+        var yv = Math.floor(Math.random() * (-5 - -15 + 1)) + -15;
+        newPart = particles.push(new Particle(x, y, w, h, color, xv, yv, g));
+        explosion.push(newPart);
+    }
+
+    return explosion;
+}
+
 let bullets = [];
 
-let enemies = [new Enemy(700,300,250,250,100)];
+let enemies = [new Enemy(700, 300, 100, 100, 100)];
+
+let particles = [];
 
 function update() {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -135,49 +160,58 @@ function update() {
     grd.addColorStop(1, "white");
     //ctx.fillStyle = '#268b07';
     ctx.fillStyle = grd;
-    ctx.fillRect(ctx.canvas.width/4, ctx.canvas.height/2,ctx.canvas.width,ctx.canvas.height/2);
+    ctx.fillRect(ctx.canvas.width / 4, ctx.canvas.height / 2, ctx.canvas.width, ctx.canvas.height / 2);
 
-    if(shootDown && shootCoolDown <= 0) {
-        bullets.push(new Bullet(100,100,crosshairX, crosshairY));
+    if (shootDown && shootCoolDown <= 0) {
+        bullets.push(new Bullet(100, 100, crosshairX, crosshairY));
         shootCoolDown = shootCoolDownTime;
     }
 
     shootCoolDown--;
 
-    for(e=0;e<enemies.length;e++){
+    for (e = 0; e < enemies.length; e++) {
         enemy = enemies[e];
         enemy.draw(ctx);
         enemy.update();
     }
 
-    for(b=0;b<bullets.length;b++){
+    for (b = 0; b < bullets.length; b++) {
         bullet = bullets[b];
         bullet.draw(ctx);
         bullet.update();
 
         enemyCollisionIndex = enemies.findIndex(enemy => bullet.collision(enemy));
 
-        if(enemyCollisionIndex !== -1){
-            enemies[enemyCollisionIndex].health -= 10;
+        if (enemyCollisionIndex !== -1) {
+            collidedEnemy = enemies[enemyCollisionIndex];
+            collidedEnemy.health -= 10;
+            newExplosion = explode(1, bullet.x, bullet.y, collidedEnemy.w / 5, collidedEnemy.h / 5, collidedEnemy.color, 1);
+            console.log(newExplosion);
             bullets.splice(b, 1);
         }
 
         try {
-            if(bullet.x <= -100 || bullet.x >= ctx.canvas.width+100 || bullet.y <= -100 || bullet.y >= ctx.canvas.height+100){
+            if (bullet.x <= -100 || bullet.x >= ctx.canvas.width + 100 || bullet.y <= -100 || bullet.y >= ctx.canvas.height + 100) {
                 bullets.splice(b, 1);
             }
-        } catch {}
+        } catch { }
+    }
+
+    for (p = 0; p < particles.length; p++) {
+        particle = particles[p];
+        particle.draw(ctx);
+        particle.physics();
     }
 
     ctx.fillStyle = '#000';
-    ctx.fillRect(crosshairX,crosshairY-50, 5, 100);
-    ctx.fillRect(crosshairX-50, crosshairY, 100, 5);
+    ctx.fillRect(crosshairX, crosshairY - 50, 5, 100);
+    ctx.fillRect(crosshairX - 50, crosshairY, 100, 5);
 
     crosshairX = mouseX;
     crosshairY = mouseY;
 
-    if(crosshairX <= ctx.canvas.width/4){
-        crosshairX = ctx.canvas.width/4;
+    if (crosshairX <= ctx.canvas.width / 4) {
+        crosshairX = ctx.canvas.width / 4;
     }
 
 }
@@ -195,13 +229,13 @@ document.addEventListener('mousemove', function (event) {
     mouseY = event.clientY;
 });
 
-document.addEventListener('mousedown', function(event){
+document.addEventListener('mousedown', function (event) {
 
     shootDown = true;
 
 });
 
-document.addEventListener('mouseup', function(event){
+document.addEventListener('mouseup', function (event) {
 
     shootDown = false;
 
